15c5165b54309bc28885b7e031460c0b887bae62
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2023-08-18 19:36:32 +0500

    prepare to deploy

--- core/settings.py
+++ core/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
